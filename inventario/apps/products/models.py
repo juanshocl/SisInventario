ba437d5411ef24f374eb087da55a7c129a6307c9
Juan Muñoz <cl.jmunoz@gmail.com>
2019-12-13 14:47:55 -0300

    Creacion de modelos de la base de datos

--- inventario/apps/products/models.py
+++ inventario/apps/products/models.py
@@ -6,16 +6,22 @@ from django.db import models
 
 # Create your models here.
 
-class products(models.Model):
+class phones(models.Model):
+    PHONETYPES = (
+        (1,'Fijo'),
+        (2,'Movil'),
+        (3,'Comercial'),
+    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name =  models.CharField(max_length=20)
-    quantity = models.IntegerField(max_length=None)
-    buy_price = models.DecimalField(max_digits=5, decimal_places=2)
-    sale_price = models.DecimalField( max_digits=5, decimal_places=2)
-    categorie_id = models.CharField(max_length=50)
-    #media_id
-    date = models.DateField(auto_now=False, auto_now_add=False)
+    number = models.CharField(max_length=14)
+    type = models.IntegerField(choices=PHONETYPES)
 
+    def __str__(self):
+        return self.id
+
+class state(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name =  models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -27,4 +33,39 @@ class category(models.Model):
     def __str__(self):
         return self.description
 
+class provider(models.Model):
+    id = models.CharField(max_length=50)
+    businessname = models.CharField(max_length=50)
+    categoryProvider = models.ForeignKey(category, on_delete=models.CASCADE, default=None)
+    address = models.CharField(max_length=50)
+    phonesProvider = models.ForeignKey(phones, on_delete=models.CASCADE, default=None)
+    stateProvider = models.ForeignKey(state, on_delete=models.CASCADE, default=None)
+    credit = models.BooleanField()
+    amount = models.IntegerField()
+
+class photos(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    thumbnail = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    bigsize = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+
+class warehouse(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    descriptionWarehouse = models.CharField( max_length=50)
+    MinimumStock  = models.IntegerField()
+
+class products(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    productProvider = models.ForeignKey(provider, on_delete=models.CASCADE, default=None)
+    description =  models.CharField(max_length=20)
+    creationDate = models.DateField(auto_now=True, auto_now_add=False)
+    lastUpate = models.DateField(auto_now=True, auto_now_add=False)
+    buy_price = models.DecimalField(max_digits=5, decimal_places=2)
+    sale_price = models.DecimalField( max_digits=5, decimal_places=2)
+    categoryProduct = models.ForeignKey(category, on_delete=models.CASCADE, default=None)
+    photos = models.ForeignKey(photos, on_delete=models.CASCADE)
+    warehouseProduct = models.ForeignKey(warehouse, on_delete=models.CASCADE, default = None)
+    isActive = models.BooleanField()
+
+    def __str__(self):
+        return self.description
     
